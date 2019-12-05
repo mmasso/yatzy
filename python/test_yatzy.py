e0644a4aa7b25d9c736c4c7bab24ccfc0fe08635
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-05 19:07:35 +0100

    refactor(test_yatzy): Quitar lista

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -10,11 +10,9 @@ def test_chance_scores_sum_of_all_dice():
 
 
 def test_yatzy_scores_50():
-    expected = 50
-    actual = Yatzy.yatzy([4, 4, 4, 4, 4])
-    assert expected == actual
-    assert 50 == Yatzy.yatzy([6, 6, 6, 6, 6])
-    assert 0 == Yatzy.yatzy([6, 6, 6, 6, 3])
+    assert 50 == Yatzy.yatzy(4, 4, 4, 4, 4)
+    assert 50 == Yatzy.yatzy(6, 6, 6, 6, 6)
+    assert 0 == Yatzy.yatzy(6, 6, 6, 6, 3)
 
 
 def test_1s():
