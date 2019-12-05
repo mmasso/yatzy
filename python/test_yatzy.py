347965a961042806b43f5ed6c82b36d03d12cecc
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-05 18:18:47 +0100

    refactor(test_chance): Refactorizar assert

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -4,9 +4,7 @@ from yatzy import Yatzy
 # available from http://pytest.org/
 
 def test_chance_scores_sum_of_all_dice():
-        expected = 15
-        actual = Yatzy.chance(2,3,4,5,1)
-        assert expected == actual
+        assert 15 == Yatzy.chance(2,3,4,5,1)
         assert 16 == Yatzy.chance(3,3,4,5,1)
   
 
