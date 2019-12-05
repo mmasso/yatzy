7f0792237ac269a29f08c0124775d5b6e10898c5
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-05 19:20:22 +0100

    refactor(ones): Cambiar nombre variable y añadir bucle

--- python/yatzy.py
+++ python/yatzy.py
@@ -17,20 +17,12 @@ class Yatzy:
         return 50
 
     @staticmethod
-    def ones(d1,  d2,  d3,  d4,  d5):
-        sum = 0
-        if (d1 == 1):
-            sum += 1
-        if (d2 == 1):
-            sum += 1
-        if (d3 == 1):
-            sum += 1
-        if (d4 == 1):
-            sum += 1
-        if (d5 == 1):
-            sum += 1
-
-        return sum
+    def ones(*dice):
+        score = 0
+        for die in dice:
+            if die == 1:
+                score += die
+        return score
 
     @staticmethod
     def twos(d1,  d2,  d3,  d4,  d5):
