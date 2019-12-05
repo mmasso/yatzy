d4e7ee7689a2734a1a07354f73987061029364d9
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-05 17:59:44 +0100

    refactor(chance): Refactorizar funcion chance
    Incluimos bucle for con variables multiples.

--- python/yatzy.py
+++ python/yatzy.py
@@ -1,13 +1,10 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
+    def chance(*dados):
         total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
+        for valor_dado in list(dados):
+            total += valor_dado
         return total
 
     @staticmethod
